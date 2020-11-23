be52fe7013788ef60073e7c6ad7e081fd33e85dd
JCxYIS <jsn811@gmail.com>
2020-11-23 22:38:37 +0800

    import materials lmao

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 from PIL import Image
+import os
 
 ######################################################
+
 print('hell world');
+
+picpath = os.path.join( os.path.dirname(__file__),  r'pic/cut19_worldend-2400x1602.jpg');
+img = Image.open(picpath);
+img.show();
---
+++ src/pic/readme.txt
@@ -0,0 +1,4 @@
+Pictures are from: 
+https://nurse-web.jp/photo/
+
+1%的場合能用到 這裡就是那 1% (笑
