b12edba3af7fd94fcace7c48d5791e54f8ea3b79
JCxYIS <jsn811@gmail.com>
2021-01-01 16:55:12 +0800

    go go rich menu

--- src/response.py
+++ src/response.py
@@ -15,7 +15,7 @@ def determine_response(myuser:User, message:str, attachmentPath:str, attachmentE
     myuser: my user
     meesage: message, may be ''
     attachmentPath: abs path, may be ''
-    attachmentExt: extension of attachment, maybe ''
+    attachmentExt: extension of attachment (jpg, mp4, m4a), may be ''
     """    
 
     # 起始
@@ -44,7 +44,7 @@ def determine_response(myuser:User, message:str, attachmentPath:str, attachmentE
             myuser.edit_pic_editions = []
             # TODO 附加quick reply之類的
             return response_templates.flex_acoustic_message( 
-                '上傳成功', '好耶', '接下來來修圖吧！', temp_path_to_server_path(attachmentPath) )
+                '上傳成功', '好耶，接下來來修圖吧！', 'rqeqeeqe', temp_path_to_server_path(attachmentPath) )
     
     # 選擇功能
     elif myuser.state == 110:
@@ -77,6 +77,30 @@ def determine_response(myuser:User, message:str, attachmentPath:str, attachmentE
 
     # default fallback
     return generate_response_from_directories('default')
+
+
+# -----------------------------------------------------------------------------------------------------------------
+
+
+
+
+def determine_attach_rich_menus(myuser:User):
+    """
+    依照現在的 user state決定要附加甚麼rich menu \\ 
+    如果沒有，回傳None
+    """
+
+    # 調整文字位置
+    if myuser.state == 112:
+        return 'richmenu-95adfde2e2fe64784441903bfb09fe2a'
+
+    else:
+        return None
+
+
+
+
+
 
 
 # -----------------------------------------------------------------------------------------------------------------
